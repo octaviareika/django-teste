0853a4a5e6e0113377febd1493f0f55962aa60f7
Cecilia <ceciliaperet81@gmail.com>
2026-06-28 20:18:06 -0300

    roteiro 2

--- home/views.py
+++ home/views.py
@@ -6,3 +6,7 @@ from .models import Mensagem
 def index(request):
     mensagens = Mensagem.objects.all()
     return render(request, "home/index.html", {"mensagens": mensagens})
+
+
+def sobre(request):                                   # ← nova view
+    return render(request, "home/sobre.html")
